0cf422b3b13684a04e3f57ac2c9cffacbde48c2f
Martin Schoeberl <martin@jopdesign.com>
2020-12-22 18:51:53 +0100

    Play with my own external library

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
 
 scalaVersion := "2.12.12"
 
+libraryDependencies += "io.github.schoeberl" % "elfscala" % "0.1"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
+import elfscala._
+
 object Main extends App {
   println("Hello, World!")
+
+  val X = 1
+  val Y = 2
+  val Z = "Hello"
+
+  println("Elf version = " + Elf.version)
 }
